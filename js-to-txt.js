a570f05c83d8297dce34771c21191f7b5b8b655d
Bryan Jennings <bryanjenningz@gmail.com>
2016-06-24 03:27:32 -0700

    Create js-to-txt.js

---
+++ js-to-txt.js
@@ -0,0 +1,40 @@
+// This program converts all the .js files in the current directory to
+// .txt files so that you can send them over Facebook chat.
+// If you want to convert the .txt files back to .js files, add the 
+// --txt-to-js argument.
+
+var fs = require('fs')
+var readline = require('readline')
+var programFilename = process.argv[1].match(/[\/\\]([^\/\\]+)$/)[1]
+var extensionRegex = /(\.[^\.]+)$/
+var toJS = process.argv.indexOf('--txt-to-js') >= 0
+var [fromExtension, toExtension] = toJS ? ['.txt', '.js'] : ['.js', '.txt']
+
+var isFileType = type => filename => {
+  var match
+  var extension = (match = filename.match(extensionRegex)) && match[1]
+  return extension === type && filename !== programFilename
+}
+
+var filenames = fs.readdirSync('.').filter(isFileType(fromExtension))
+
+if (filenames.length > 0) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question(`Files: ${filenames.join('\n       ')}\nConverting these files from ${fromExtension} to ${toExtension}. Is that okay? (y/n) `, answer => {
+    if (answer.toLowerCase() === 'y') {
+      console.log(`Converting ${fromExtension} files to ${toExtension} files...`)
+      filenames.forEach(filename => 
+        fs.renameSync(filename, filename.replace(extensionRegex, toExtension)))
+      console.log('Done converting files!')
+    } else {
+      console.log('Not converting any files.')
+    }
+    rl.close()
+  })
+} else {
+  console.log(`No ${fromExtension} files to convert in your current directory.`)
+}
